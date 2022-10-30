ba7f4af27bb3e9f7121f98ef1d76b1cbd6c2262a
E117_ <e117@ScriptKiddie>
2022-10-30 17:10:19 +0000

    first commit

--- integer_to_text.py
+++ integer_to_text.py
@@ -3,7 +3,7 @@ from Crypto.Util.number import *
 a = input("Enter integers here:")
 
 print(hex(int(a)));
-#we get 0x
+#we get 0x63727970746f7b336e633064316e365f346c6c5f3768335f7734795f6430776e7d
 #remove the 0x
 text = bytes.fromhex(a).decode("ASCII")
 print(text)

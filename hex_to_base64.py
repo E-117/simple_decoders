79ac15709f751f7d84e23d32bf67d09ea12e168e
E117_ <e117@ScriptKiddie>
2022-10-30 12:46:27 +0000

    first commit

---
+++ hex_to_base64.py
@@ -0,0 +1,10 @@
+import binascii
+import base64
+hex_string = input("Enter your hex string: ");
+a = bytes.fromhex(hex_string)
+
+b = a;
+text = base64.b64encode(b)
+print(text)
+
+base64_string = input("Enter your base64 string:")
